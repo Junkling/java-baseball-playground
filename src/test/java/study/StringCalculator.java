34ff1fdab3eb19cca1a86e9709d06afd14e001bb
(Jun) <(ddoly0106@gmail.com)>
2023-02-08 17:40:50 +0900

    문자열 계산기

--- src/test/java/study/StringCalculator.java
+++ src/test/java/study/StringCalculator.java
@@ -1,2 +1,48 @@
-package study;public class StringCalculator {
+package study;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StringCalculator {
+    @Test
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] split = scanner.nextLine().split(" ");
+        int length = split.length;
+        int[] numArr = new int[(length / 2)+1];
+        String[] strArr = new String[length / 2];
+
+        for (int i = 0; i <length ; i++) {
+            if (i % 2 == 0) {
+                numArr[i/2]=Integer.parseInt(split[i]);
+            }
+            if(i%2==1){
+                strArr[i / 2] = split[i];
+            }
+        }
+        int sum = numArr[0];
+        for (int i = 0; i < length / 2; i++) {
+            if (strArr[i].equals("+")) {
+                sum = sum + numArr[i + 1];
+                System.out.println("sum = " + sum);
+            }
+            if (strArr[i].equals("-")) {
+                sum = sum - numArr[i + 1];
+                System.out.println("sum = " + sum);
+            }
+            if (strArr[i].equals("*")) {
+                sum = sum * numArr[i + 1];
+                System.out.println("sum = " + sum);
+            }
+            if (strArr[i].equals("/")) {
+                sum = sum / numArr[i + 1];
+                System.out.println("sum = " + sum);
+            }
+        }
+        Assertions.assertEquals(sum, 10);
+    }
 }
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,17 @@
 package study;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StringTest {
     @Test
@@ -10,4 +19,48 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+    @Test
+    void split() {
+        String[] actual = "1,2".split(",");
+        assertThat(actual).containsExactly("1", "2");
+    }
+
+    @Test
+    void substring() {
+        String actual = "(1,2)".substring(1, 4);
+        assertThat(actual).isEqualTo("1,2");
+    }
+    @Test
+    void charAt() {
+        char actual = "abc".charAt(2);
+        assertThat(actual).isEqualTo('c');
+        assertThatThrownBy(() -> {
+            char outRange = "abc".charAt(3);
+        }).isInstanceOf(IndexOutOfBoundsException.class);
+    }
+
+    public Set<Integer> numbers;
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void setTest() {
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2})
+    void setTest2(int num) {
+        assertTrue(numbers.contains(num));
+    }
+
 }
